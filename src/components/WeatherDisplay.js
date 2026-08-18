4cf61a3943e4708e6e7ec9b34e852977a29ad949
Supra-Mk4-1998 <sr7773879@gmail.com>
2026-08-18 16:07:55 +0530

    push 1

---
+++ src/components/WeatherDisplay.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function WeatherDisplay({ weather }) {
+  const temperatureColor =
+    weather.temperature > 20 ? "red" : "blue";
+
+  return (
+    <div>
+      <p>
+        Temperature:{" "}
+        <span style={{ color: temperatureColor }}>
+          {weather.temperature}
+        </span>
+      </p>
+
+      <p>
+        Conditions: {weather.conditions}
+      </p>
+    </div>
+  );
+}
+
+export default WeatherDisplay;
